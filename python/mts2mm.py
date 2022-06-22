c81fba87ce339146650cca9a06c374c4f3dc93ba
FerMarAlmeida <107759677+FerMarAlmeida@users.noreply.github.com>
2022-06-22 01:06:09 -0300

    Update mts2mm.py

--- python/mts2mm.py
+++ python/mts2mm.py
@@ -1,5 +1,5 @@
 ## Escreva um programa que leia um valor em metros e o exiba convertido em milímetros.
 
-metros = float(input("Digite o valor em métros: "))
+metros = float(input("Digite o valor em metros: "))
 milímetros = metros * 1000
 print("%10.3f metros equivalem a %10.3f milímetros." % (metros, milímetros))
