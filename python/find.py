b68fc5dc3e5cbef9a11aae0f5db699a909d751b5
FerMarAlmeida <107759677+FerMarAlmeida@users.noreply.github.com>
2022-06-22 02:00:52 -0300

    Create find.py

---
+++ python/find.py
@@ -0,0 +1,14 @@
+# Escreva um programa que leia duas strings. Verifique se a segunda ocorre dentro da primeira e imprima a posição de início.
+# 1ª string: AABBEFAATT
+# 2ª string: BE
+# Resultado: BE encontrado na posição 3 de AABBEFAATT
+
+primeira = input("Digite a primeira string: ")
+segunda = input("Digite a segunda string: ")
+
+posição = primeira.find(segunda)
+
+if posição == -1:
+    print(f"'{segunda}' não encontrada em '{primeira}'")
+else:
+    print(f"{segunda} encontrada na posição {posição} de {primeira}")
